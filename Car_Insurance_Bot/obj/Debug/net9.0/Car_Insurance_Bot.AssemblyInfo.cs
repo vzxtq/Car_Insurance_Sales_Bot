ed5578ff7c6bf2e1fb8704199b2cf242ad5d0ef7
vzxtq <fixitex0206@gmail.com>
2025-04-24 11:44:24 +0200

    docker add

--- Car_Insurance_Bot/obj/Debug/net9.0/Car_Insurance_Bot.AssemblyInfo.cs
+++ Car_Insurance_Bot/obj/Debug/net9.0/Car_Insurance_Bot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Car_Insurance_Bot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecb261c698b6d208d1e3ffaa9c44a1aa9758b94e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03fac01dbf569351bc0b2ea81c1df7c4d5b441bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Car_Insurance_Bot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Car_Insurance_Bot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
